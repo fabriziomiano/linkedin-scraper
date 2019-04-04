31fe00cee1db4917c65d6ec3e3a4a56d6f9d6452
Fabrizio Miano <fabriziomiano@gmail.com>
2019-04-04 10:36:45 +0200

    Fix scroll command to parse the right soup in get_languages()

--- classes/UserScraper.py
+++ classes/UserScraper.py
@@ -172,9 +172,9 @@ class UserScraper(object):
                     "document.body.style.zoom='50%'")
                 sleep(3)
                 skills = self.get_skills()
-                soup = bs(self.driver.page_source, 'html.parser')
                 degree = self.get_degree(soup)
                 scroll_profile_page(self.driver)
+                soup = bs(self.driver.page_source, 'html.parser')
                 languages = self.get_languages(soup)
                 user_data = {
                     "URL": url,
